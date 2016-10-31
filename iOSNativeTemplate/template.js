fdd89d79b46276e4fc0283e323069575bae10a7f
Wolfgang Mathurin <wmathurin@salesforce.com>
2016-10-31 16:16:06 -0700

    Adding copyright and some comments Adding install.sh

--- iOSNativeTemplate/template.js
+++ iOSNativeTemplate/template.js
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2016-present, salesforce.com, inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ * following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * This script is called from forceios to inject app name, company id, org name etc in the template
+ */
+
 module.exports.prepare = function(config, replaceInFiles, moveFile, runProcessThrowError) {
 
     var path = require('path');
@@ -46,7 +76,7 @@ module.exports.prepare = function(config, replaceInFiles, moveFile, runProcessTh
     //
     // Rename files
     //
-    moveFile(templateSchemeFile, path.join(config.appname + '.xcodeproj', 'xcshareddata', 'xcschemes', config.appname + '.xcscheme'));
+    moveFile(templateSchemeFile, path.join(templateAppName + '.xcodeproj', 'xcshareddata', 'xcschemes', config.appname + '.xcscheme'));
     moveFile(templatePrefixFile, path.join(templateAppName, config.appname + '-Prefix.pch'));
     moveFile(templateInfoFile, path.join(templateAppName, config.appname + '-Info.plist'));
     moveFile(templateEntitlementsFile, path.join(templateAppName, config.appname + '.entitlements'));
@@ -54,8 +84,7 @@ module.exports.prepare = function(config, replaceInFiles, moveFile, runProcessTh
     moveFile(templateAppName, config.appname);
 
     //
-    // Run npm and pod
+    // Run install.sh
     //
-    runProcessThrowError('npm install');
-    runProcessThrowError('pod install');
+    runProcessThrowError('install.sh');
 };
