ab700c204ad7699ff9572ed88a384732e3ec086e
Wolfgang Mathurin <wmathurin@salesforce.com>
2016-10-31 15:00:09 -0700

    Script to be run by forceios to use correct app name, org name etc in template

---
+++ iOSNativeTemplate/template.js
@@ -0,0 +1,56 @@
+module.exports.prepare = function(config, replaceInFiles, moveFile, runProcessThrowError) {
+
+    var path = require('path');
+
+    // Values in template
+    var templateAppName = 'iOSNativeTemplate';
+    var templateCompanyId = 'com.salesforce.iosnativetemplate';
+    var templateOrgName = 'iOSNativeTemplateOrganizationName';
+    var tempalteAppId = '3MVG9Iu66FKeHhINkB1l7xt7kR8czFcCTUhgoA8Ol2Ltf1eYHOU4SqQRSEitYFDUpqRWcoQ2.dBv_a1Dyu5xa';
+    var templateCallbackUri = 'testsfdc =///mobilesdk/detect/oauth/done';
+
+    // Key files
+    var templatePodfile = 'Podfile';
+    var templatePackageFile = 'package.json';
+    var templateProjectDir = templateAppName + '.xcodeproj';
+    var templateProjectFile = path.join(templateProjectDir, 'project.pbxproj');
+    var templateSchemeFile = path.join(templateAppName + '.xcodeproj', 'xcshareddata', 'xcschemes', templateAppName + '.xcscheme');
+    var templatePrefixFile = path.join(templateAppName, templateAppName + '-Prefix.pch');
+    var templateInfoFile = path.join(templateAppName, templateAppName + '-Info.plist');
+    var templateEntitlementsFile = path.join(templateAppName, templateAppName + '.entitlements');
+    var templateAppDelegateFile = path.join(templateAppName, 'AppDelegate.m');
+
+    //
+    // Replace in files
+    //
+
+    // app name
+    replaceInFiles(templateAppName, config.appname, [templatePodfile, templatePackageFile, templateProjectFile, templateSchemeFile, templateEntitlementsFile, templateAppDelegateFile]);
+
+    // company id
+    replaceInFiles(templateCompanyId, config.companyid, [templateInfoFile, templateEntitlementsFile]);
+
+    // org name
+    replaceInFiles(templateOrgName, config.orgname, [templateProjectFile]);
+
+    // app id
+    replaceInFiles(templateAppId, config.appid, [templateAppDelegateFile]);
+                   
+    // callback uri
+    replaceInFiles(templateCallbackUri, config.callbackuri, [templateAppDelegateFile]);
+
+    //
+    // Rename files
+    //
+    moveFile(templateSchemeFile, path.join(config.appname + '.xcodeproj', 'xcshareddata', 'xcschemes', config.appname + '.xcscheme'));
+    moveFile(templatePrefixFile, config.appname + '-Prefix.pch');
+    moveFile(templateInfoFile, config.appname + '-Info.plist');
+    moveFile(templateEntitlementsFile, config.appname + '.entitlements');
+    moveFile(templateProjectDir, config.appname + '.xcodeproj');
+
+    //
+    // Run npm and pod
+    //
+    runProcessThrowError('npm install');
+    runProcessThrowError('pod install');
+};
