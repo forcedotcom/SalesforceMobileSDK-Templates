31e2dac9dc0f79863e9c80a9c2fb524c9db66ff6
Wolfgang Mathurin <wmathurin@salesforce.com>
2017-06-30 19:10:53 -0600

    Sharing RN template js code between iOS and Android - using new navigation ES6 code

--- ReactNativeTemplate/index.ios.js
+++ ReactNativeTemplate/index.ios.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-present, salesforce.com, inc.
+ * Copyright (c) 2017-present, salesforce.com, inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided
@@ -24,132 +24,8 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-'use strict';
+import { AppRegistry } from 'react-native';
+import { App } from './app.js';
 
-import React from 'react';
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    ListView,
-    PixelRatio,
-    NavigatorIOS
-} from 'react-native';
-
-import {oauth, net} from 'react-native-force';
-
-var App = React.createClass({
-    getInitialState: function() {
-        return {
-            authenticated: false
-        };
-    },
-
-    componentDidMount: function() {
-        var that = this;
-        oauth.getAuthCredentials(
-            function() {
-                that.setState({authenticated:true});
-            },
-            function(error) {
-                console.log('Failed to authenticate:' + error);
-            }
-        );
-    },
-
-    render: function() {
-        if (!this.state.authenticated)
-            return (<View/>); // Show splash screen if you have one
-
-        return (
-            <NavigatorIOS
-                style={styles.container}
-                initialRoute={{
-                    title: 'Mobile SDK Sample App',
-                    component: UserList,
-                }}
-            />
-        );
-    }
-});
-
-var UserList = React.createClass({
-    getInitialState: function() {
-      var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      return {
-          dataSource: ds.cloneWithRows([]),
-      };
-    },
-    
-    componentDidMount: function() {
-        var that = this;
-        var soql = 'SELECT Id, Name FROM User LIMIT 10';
-        net.query(soql,
-                  function(response) {
-                      var users = response.records;
-                      var data = [];
-                      for (var i in users) {
-                          data.push(users[i]["Name"]);
-                      }
-
-                      that.setState({
-                          dataSource: that.getDataSource(data),
-                      });
-
-                  });
-    },
-
-    getDataSource: function(users: Array<any>): ListViewDataSource {
-        return this.state.dataSource.cloneWithRows(users);
-    },
-
-    render: function() {
-        return (
-            <ListView
-              dataSource={this.state.dataSource}
-              renderRow={this.renderRow}
-              enableEmptySections={true}
-            />
-      );
-    },
-
-    renderRow: function(rowData: Object) {
-        return (
-                <View>
-                    <View style={styles.row}>
-                      <Text numberOfLines={1}>
-                       {rowData}
-                      </Text>
-                    </View>
-                    <View style={styles.cellBorder} />
-                </View>
-        );
-    }
-});
-
-var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-    },
-    header: {
-        height: 50,
-        alignItems:'center'
-    },
-    row: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: 'white',
-        flexDirection: 'row',
-        padding: 12,
-    },
-    cellBorder: {
-        backgroundColor: 'rgba(0, 0, 0, 0.1)',
-        // Trick to get the thinest line the device can display
-        height: 1 / PixelRatio.get(),
-        marginLeft: 4,
-    },
-});
 
 AppRegistry.registerComponent('ReactNativeTemplate', () => App);
