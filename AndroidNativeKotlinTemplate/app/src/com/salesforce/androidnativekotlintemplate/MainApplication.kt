be999ae533e1bd31e5e9b093ccef694c2cde398d
Bharath Hariharan <bhariharan@salesforce.com>
2017-10-30 20:29:02 -0700

    Updating Kotlin template for IDP flow

--- AndroidNativeKotlinTemplate/app/src/com/salesforce/androidnativekotlintemplate/MainApplication.kt
+++ AndroidNativeKotlinTemplate/app/src/com/salesforce/androidnativekotlintemplate/MainApplication.kt
@@ -42,6 +42,14 @@ class MainApplication : Application() {
         SmartSyncSDKManager.initNative(applicationContext, NativeKeyImpl(), MainActivity::class.java)
 
         /*
+         * Uncomment the following lines to enable IDP login flow. This will allow the user to
+         * either authenticate using the current app or use a designated IDP app for login.
+         * Replace 'idpAppURIScheme' with the URI scheme of the IDP app meant to be used.
+         */
+        // SalesforceSDKManager.getInstance().setIDPLoginFlowEnabled(true);
+        // SalesforceSDKManager.getInstance().setIDPAppURIScheme(idpAppURIScheme);
+
+        /*
          * Uncomment the following line to enable browser based login. This will use a
          * Chrome custom tab to login instead of the default WebView. You will also need
          * to uncomment a few lines of code in SalesforceSDK library project's AndroidManifest.xml.
