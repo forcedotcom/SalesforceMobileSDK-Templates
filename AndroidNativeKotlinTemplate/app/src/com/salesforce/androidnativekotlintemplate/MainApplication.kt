5b9837a4e97613c2ac84197ffa1b846378516c19
Bharath Hariharan <bhariharan@salesforce.com>
2017-11-13 14:46:28 -0800

    Removing setter for IDP login flow

--- AndroidNativeKotlinTemplate/app/src/com/salesforce/androidnativekotlintemplate/MainApplication.kt
+++ AndroidNativeKotlinTemplate/app/src/com/salesforce/androidnativekotlintemplate/MainApplication.kt
@@ -42,11 +42,10 @@ class MainApplication : Application() {
         SmartSyncSDKManager.initNative(applicationContext, NativeKeyImpl(), MainActivity::class.java)
 
         /*
-         * Uncomment the following lines to enable IDP login flow. This will allow the user to
+         * Uncomment the following line to enable IDP login flow. This will allow the user to
          * either authenticate using the current app or use a designated IDP app for login.
          * Replace 'idpAppURIScheme' with the URI scheme of the IDP app meant to be used.
          */
-        // SmartSyncSDKManager.getInstance().isIDPLoginFlowEnabled = true
         // SmartSyncSDKManager.getInstance().idpAppURIScheme = idpAppURIScheme
 
 
--- AndroidNativeTemplate/app/src/com/salesforce/androidnativetemplate/MainApplication.java
+++ AndroidNativeTemplate/app/src/com/salesforce/androidnativetemplate/MainApplication.java
@@ -43,11 +43,10 @@ public class MainApplication extends Application {
 		SmartSyncSDKManager.initNative(getApplicationContext(), new NativeKeyImpl(), MainActivity.class);
 
         /*
-         * Uncomment the following lines to enable IDP login flow. This will allow the user to
+         * Uncomment the following line to enable IDP login flow. This will allow the user to
          * either authenticate using the current app or use a designated IDP app for login.
          * Replace 'idpAppURIScheme' with the URI scheme of the IDP app meant to be used.
          */
-		// SmartSyncSDKManager.getInstance().setIDPLoginFlowEnabled(true);
 		// SmartSyncSDKManager.getInstance().setIDPAppURIScheme(idpAppURIScheme);
 
         /*
