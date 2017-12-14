beb0ac9f3f128e6796099fe15c703b32af0e2189
trooper2013 <>
2017-12-13 17:01:24 -0800

    Added Post init block

--- iOSNativeSwiftTemplate/iOSNativeSwiftTemplate/AppDelegate.swift
+++ iOSNativeSwiftTemplate/iOSNativeSwiftTemplate/AppDelegate.swift
@@ -40,16 +40,18 @@ class AppDelegate : UIResponder, UIApplicationDelegate
     {
         super.init()
         
-        //Uncomment the following line inorder to enable/force the use of advanced authentication flow.
-        // SFUserAccountManager.sharedInstance().advancedAuthConfiguration = SFOAuthAdvancedAuthConfiguration.require;
-        // OR
-        // To  retrieve advanced auth configuration from the org, to determine whether to initiate advanced authentication.
-        // SFUserAccountManager.sharedInstance().advancedAuthConfiguration = SFOAuthAdvancedAuthConfiguration.allow;
-        
-        // NOTE: If advanced authentication is configured or forced,  it will launch Safari to handle authentication
-        // instead of a webview. You must implement application:openURL:options  to handle the callback.
-        
-        SalesforceSwiftSDKManager.initSDK().Builder.configure { (appconfig: SFSDKAppConfig) -> Void in
+        SalesforceSwiftSDKManager.initSDK()
+        .postInit {
+            //Uncomment the following line inorder to enable/force the use of advanced authentication flow.
+            // SFUserAccountManager.sharedInstance().advancedAuthConfiguration = SFOAuthAdvancedAuthConfiguration.require;
+            // OR
+            // To  retrieve advanced auth configuration from the org, to determine whether to initiate advanced authentication.
+            // SFUserAccountManager.sharedInstance().advancedAuthConfiguration = SFOAuthAdvancedAuthConfiguration.allow;
+            
+            // NOTE: If advanced authentication is configured or forced,  it will launch Safari to handle authentication
+            // instead of a webview. You must implement application:openURL:options  to handle the callback.
+        }
+        .Builder.configure { (appconfig: SFSDKAppConfig) -> Void in
             appconfig.oauthScopes = ["web", "api"]
             appconfig.remoteAccessConsumerKey = RemoteAccessConsumerKey
             appconfig.oauthRedirectURI = OAuthRedirectURI
