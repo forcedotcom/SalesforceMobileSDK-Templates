d68b022f18c35d87d8c1f0e048e20228a56d9905
Wolfgang Mathurin <wmathurin@salesforce.com>
2019-01-15 14:48:10 -0800

    Improved setversion.sh

--- setversion.sh
+++ setversion.sh
@@ -3,46 +3,35 @@
 #set -x
 
 OPT_VERSION=""
+OPT_IS_DEV="no"
+RED='\033[0;31m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
 usage ()
 {
     echo "Use this script to set Mobile SDK version number in source files"
-    echo "Usage: $0 -v <versionName e.g. 7.1.0> [-d <isDev e.g. yes>]"
+    echo "Usage: $0 -v <version> [-d <isDev>]"
+    echo "  where: version is the version e.g. 7.1.0"
+    echo "         isDev is yes or no (default) to indicate whether it is a dev build"
 }
 
 parse_opts ()
 {
-    while getopts v: command_line_opt
+    while getopts v:d: command_line_opt
     do
         case ${command_line_opt} in
-            v)
-                OPT_VERSION=${OPTARG};;
-            ?)
-                echo "Unknown option '-${OPTARG}'."
-                usage
-                exit 1;;
+            v)  OPT_VERSION=${OPTARG};;
+            d)  OPT_IS_DEV=${OPTARG};;
         esac
     done
 
     if [ "${OPT_VERSION}" == "" ]
     then
-        echo "You must specify a value for the version."
+        echo -e "${RED}You must specify a value for the version.${NC}"
         usage
         exit 1
     fi
-
-    valid_version_regex='^[0-9]+\.[0-9]+\.[0-9]+$'
-    if [[ "${OPT_VERSION}" =~ $valid_version_regex ]]
-     then
-         # No action
-            :
-     else
-        echo "${OPT_VERSION} is not a valid version name.  Should be in the format <integer.integer.interger>"
-        exit 2
-    fi
-
 }
 
 # Helper functions
@@ -50,23 +39,34 @@ update_package_json ()
 {
     local file=$1
     local version=$2
-    sed -i "s/\.git\#[^\"]*\"/\.git\#v${version}\"/g" ${file}
+    sed -i "s/\.git\#[^\"]*\"/\.git\#${version}\"/g" ${file}
 }
 
 parse_opts "$@"
 
-echo -e "${YELLOW}*** SETTING VERSION TO ${OPT_VERSION}, IS DEV = ${OPT_IS_DEV} ***${NC}"
+SDK_TAG=""
+if [ "$OPT_IS_DEV" == "yes" ]
+then
+    echo "here 1"
+    SDK_TAG="dev"
+else
+    echo "here 2"
+    SDK_TAG="v${OPT_VERSION}"
+fi
+
+echo -e "${YELLOW}*** POINTING TO SDK TAG ${SDK_TAG} ***${NC}"
 
 echo "*** Updating package.json files ***"
-update_package_json "./AndroidNativeKotlinTemplate/package.json"  "${OPT_VERSION}"
-update_package_json "./iOSIDPTemplate/package.json"  "${OPT_VERSION}"
-update_package_json "./ReactNativeTemplate/package.json"  "${OPT_VERSION}"
-update_package_json "./SmartSyncExplorerSwift/package.json"  "${OPT_VERSION}"
-update_package_json "./HybridLocalTemplate/package.json"  "${OPT_VERSION}"
-update_package_json "./AndroidNativeTemplate/package.json"  "${OPT_VERSION}"
-update_package_json "./iOSNativeTemplate/package.json"  "${OPT_VERSION}"
-update_package_json "./HybridRemoteTemplate/package.json"  "${OPT_VERSION}"
-update_package_json "./AndroidIDPTemplate/package.json"  "${OPT_VERSION}"
-update_package_json "./iOSNativeSwiftTemplate/package.json"  "${OPT_VERSION}"
-update_package_json "./SmartSyncExplorerReactNative/package.json"  "${OPT_VERSION}"
+
+update_package_json "./AndroidNativeKotlinTemplate/package.json"  "${SDK_TAG}"
+update_package_json "./iOSIDPTemplate/package.json"  "${SDK_TAG}"
+update_package_json "./ReactNativeTemplate/package.json"  "${SDK_TAG}"
+update_package_json "./SmartSyncExplorerSwift/package.json"  "${SDK_TAG}"
+update_package_json "./HybridLocalTemplate/package.json"  "${SDK_TAG}"
+update_package_json "./AndroidNativeTemplate/package.json"  "${SDK_TAG}"
+update_package_json "./iOSNativeTemplate/package.json"  "${SDK_TAG}"
+update_package_json "./HybridRemoteTemplate/package.json"  "${SDK_TAG}"
+update_package_json "./AndroidIDPTemplate/package.json"  "${SDK_TAG}"
+update_package_json "./iOSNativeSwiftTemplate/package.json"  "${SDK_TAG}"
+update_package_json "./SmartSyncExplorerReactNative/package.json"  "${SDK_TAG}"
 
