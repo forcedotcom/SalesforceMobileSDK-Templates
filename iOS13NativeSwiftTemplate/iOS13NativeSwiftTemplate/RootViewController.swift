7f541c92a708aa4b5c79e90321a6059704672ffc
Wolfgang Mathurin <wmathurin@salesforce.com>
2019-08-27 15:56:49 -0700

    New template iOS13NativeSwiftTemplate (for now just a copy of iOSNativeSwiftTemplate)

---
+++ iOS13NativeSwiftTemplate/iOS13NativeSwiftTemplate/RootViewController.swift
@@ -0,0 +1,85 @@
+/*
+ Copyright (c) 2015-present, salesforce.com, inc. All rights reserved.
+ 
+ Redistribution and use of this software in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this list of conditions
+ and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of
+ conditions and the following disclaimer in the documentation and/or other materials provided
+ with the distribution.
+ * Neither the name of salesforce.com, inc. nor the names of its contributors may be used to
+ endorse or promote products derived from this software without specific prior written
+ permission of salesforce.com, inc.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import Foundation
+import UIKit
+import SalesforceSDKCore
+
+class RootViewController : UITableViewController {
+    
+    var dataRows = [Dictionary<String, Any>]()
+    
+    // MARK: - View lifecycle
+    override func loadView() {
+        super.loadView()
+        self.title = "Mobile SDK Sample App"
+        let request = RestClient.shared.request(forQuery: "SELECT Name FROM Contact LIMIT 10", apiVersion: SFRestDefaultAPIVersion)
+        
+        RestClient.shared.send(request: request, onFailure: { (error, urlResponse) in
+            SalesforceLogger.d(type(of:self), message:"Error invoking: \(request)")
+        }) { [weak self] (response, urlResponse) in
+            
+            guard let strongSelf = self,
+                let jsonResponse = response as? Dictionary<String,Any>,
+                let result = jsonResponse ["records"] as? [Dictionary<String,Any>]  else {
+                    return
+            }
+            
+            SalesforceLogger.d(type(of:strongSelf),message:"Invoked: \(request)")
+            
+            DispatchQueue.main.async {
+                strongSelf.dataRows = result
+                strongSelf.tableView.reloadData()
+            }
+        }
+    }
+    
+    // MARK: - Table view data source
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
+        return self.dataRows.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "CellIdentifier"
+        
+        // Dequeue or create a cell of the appropriate type.
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier:cellIdentifier) ?? UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        
+        // If you want to add an image to your cell, here's how.
+        let image = UIImage(named: "icon.png")
+        cell.imageView?.image = image
+        
+        // Configure the cell to show the data.
+        let obj = dataRows[indexPath.row]
+        cell.textLabel?.text = obj["Name"] as? String
+        
+        // This adds the arrow to the right hand side.
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        return cell
+    }
+}
