83f548e3ce815b079654b76027133c208885aac2
Raj Rao <rao.r@salesforce.com>
2020-02-21 15:56:49 -0800

    Fixed indents

--- iOS13NativeSwiftTemplate/iOS13NativeSwiftTemplate/SceneDelegate.swift
+++ iOS13NativeSwiftTemplate/iOS13NativeSwiftTemplate/SceneDelegate.swift
@@ -65,8 +65,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
         self.initializeAppViewState()
-         AuthHelper.loginIfRequired {
-                   self.setupRootViewController()
+        AuthHelper.loginIfRequired {
+            self.setupRootViewController()
         }
     }
 
