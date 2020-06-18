5d134d477efeadd6e16a5289768a2b298f1f9b97
Wolfgang Mathurin <wmathurin@salesforce.com>
2020-06-17 22:59:03 -0700

    Template for hybrid lwc

---
+++ HybridLwcTemplate/template.js
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2016-present, salesforce.com, inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ * following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Prepare template
+ *
+ * @return list of maps with
+ *   workspace
+ *   bootconfigFile
+ *   platform
+ */
+function prepare(config, replaceInFiles, moveFile, removeFile) {
+
+    // Dependencies
+    var path = require('path');
+
+    // Values in template
+    var templateStartPage = 'apex/HybridRemotePage';
+
+    // Key files
+    var templateBootconfigFile = path.join('bootconfig.json');
+
+    //
+    // Replace in files
+    //
+    replaceInFiles(templateStartPage, config.startpage, [templateBootconfigFile]);
+
+    //
+    // Install dependencies
+    //
+    require('./install');
+
+    //
+    // Move/remove some files
+    //
+    moveFile(path.join('mobile_sdk', 'SalesforceMobileSDK-Shared', 'libs', 'force.js'), path.join('force-app', 'main', 'staticresources', 'other', 'force.js'));
+    removeFile('node_modules');
+    removeFile('mobile_sdk');
+    removeFile('package.json');
+    removeFile('template.js');
+    removeFile('install.js');
+
+    // Return paths of workspace and file with oauth config
+    return config.platform.split(',').map(platform => {
+        return {
+            workspacePath: path.join('platforms', platform),
+            bootconfigFile: path.join('www', 'bootconfig.json'),
+            platform: platform
+        };
+    });
+}
+
+//
+// Exports
+//
+module.exports = {
+    appType: 'hybrid_lwc',
+    prepare: prepare
+};
+
