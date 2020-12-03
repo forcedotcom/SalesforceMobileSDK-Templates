6a29d88188516499b289c4867aebde354a8db1ed
Wolfgang Mathurin <wmathurin@salesforce.com>
2020-12-02 17:53:56 -0800

    Fixing other react native templates

--- ReactNativeDeferredTemplate/installios.js
+++ ReactNativeDeferredTemplate/installios.js
@@ -6,7 +6,7 @@ var path = require('path');
 var fs = require('fs');
 
 console.log('Installing npm dependencies');
-execSync('npm install', {stdio:[0,1,2]});
+execSync('yarn install', {stdio:[0,1,2]});
 
 console.log('Installing sdk dependencies');
 var sdkDependency = 'SalesforceMobileSDK-iOS';
