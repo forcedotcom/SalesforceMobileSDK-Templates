701f100399ec23bd995705d531c7f85b82df2d96
Wolfgang Mathurin <wmathurin@salesforce.com>
2020-12-09 16:35:38 -0800

    Misc typos

--- AndroidNativeTemplate/app/src/com/salesforce/androidnativetemplate/MainApplication.java
+++ AndroidNativeTemplate/app/src/com/salesforce/androidnativetemplate/MainApplication.java
@@ -50,7 +50,7 @@ public class MainApplication extends Application {
 		/*
 		 * Un-comment the line below to enable push notifications in this app.
 		 * Replace 'pnInterface' with your implementation of 'PushNotificationInterface'.
-		 * Add your Google package ID in 'bootonfig.xml', as the value
+		 * Add your Google package ID in 'bootconfig.xml', as the value
 		 * for the key 'androidPushNotificationClientId'.
 		 */
 		// MobileSyncSDKManager.getInstance().setPushNotificationReceiver(pnInterface);
--- ReactNativeDeferredTemplate/android/app/src/main/java/com/salesforce/reactnativedeferredtemplate/MainApplication.java
+++ ReactNativeDeferredTemplate/android/app/src/main/java/com/salesforce/reactnativedeferredtemplate/MainApplication.java
@@ -87,7 +87,7 @@ public class MainApplication extends Application implements ReactApplication {
         /*
 		 * Un-comment the line below to enable push notifications in this app.
 		 * Replace 'pnInterface' with your implementation of 'PushNotificationInterface'.
-		 * Add your Google package ID in 'bootonfig.xml', as the value
+		 * Add your Google package ID in 'bootconfig.xml', as the value
 		 * for the key 'androidPushNotificationClientId'.
 		 */
         // SalesforceReactSDKManager.getInstance().setPushNotificationReceiver(pnInterface);
