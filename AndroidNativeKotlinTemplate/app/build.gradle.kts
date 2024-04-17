d1d1a87bbfdb3db4bfe8db8106a06fef90311488
Brandon Page <bpage@salesforce.com>
2024-04-17 11:59:31 -0700

    Add targetSdk to template apps.

--- AndroidNativeKotlinTemplate/app/build.gradle.kts
+++ AndroidNativeKotlinTemplate/app/build.gradle.kts
@@ -15,6 +15,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
+        targetSdk = 34
         minSdk = 26
     }
 
--- MobileSyncExplorerKotlinTemplate/app/build.gradle.kts
+++ MobileSyncExplorerKotlinTemplate/app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
 
     defaultConfig {
         applicationId = "com.salesforce.mobilesyncexplorerkotlintemplate"
+        targetSdk = 34
         minSdk = 26
         versionCode = 1
         versionName = "1.0"
@@ -50,12 +51,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 
     buildFeatures {
