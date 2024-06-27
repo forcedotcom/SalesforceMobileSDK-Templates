e7d197631f11d237a29f6a13a0b83db34f5f77e7
Wolfgang Mathurin <wmathurin@salesforce.com>
2024-06-27 16:25:59 -0700

    Upgrading gradle - continued

--- AndroidNativeLoginTemplate/buildSrc/build.gradle.kts
+++ AndroidNativeLoginTemplate/buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.2.0")
+    implementation("com.android.tools.build:gradle:8.7.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
 }
