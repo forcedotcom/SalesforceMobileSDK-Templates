d1074e2ed5179a19183b15c4dc1132830a376276
Eric C. Johnson <Johnson.Eric@Salesforce.com>
2024-10-09 16:46:38 -0600

    @W-16814802: [iOS] Add Deep Link From External QR Code Reader To iOS Native Swift Template QR Code Login (Consolidate QR Code Log In Set Up Logic For Android)

--- AndroidNativeKotlinTemplate/app/src/main/java/com/salesforce/androidnativekotlintemplate/MainActivity.kt
+++ AndroidNativeKotlinTemplate/app/src/main/java/com/salesforce/androidnativekotlintemplate/MainActivity.kt
@@ -26,6 +26,9 @@
  */
 package com.salesforce.androidnativekotlintemplate
 
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
+import android.net.Uri
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.UPSIDE_DOWN_CAKE
 import android.os.Bundle
@@ -45,9 +48,14 @@ import com.salesforce.androidsdk.rest.RestClient
 import com.salesforce.androidsdk.rest.RestClient.AsyncRequestCallback
 import com.salesforce.androidsdk.rest.RestRequest
 import com.salesforce.androidsdk.rest.RestResponse
-import com.salesforce.androidsdk.ui.LoginActivity.Companion.isQrCodeLoginIntent
-import com.salesforce.androidsdk.ui.LoginActivity.Companion.uiBridgeApiParametersFromQrCodeLoginUrl
+import com.salesforce.androidsdk.ui.LoginActivity
+import com.salesforce.androidsdk.ui.LoginActivity.Companion.EXTRA_KEY_FRONTDOOR_BRIDGE_URL
+import com.salesforce.androidsdk.ui.LoginActivity.Companion.EXTRA_KEY_PKCE_CODE_VERIFIER
+import com.salesforce.androidsdk.ui.LoginActivity.Companion.isQrCodeLoginUrlIntent
+import com.salesforce.androidsdk.ui.LoginActivity.Companion.qrCodeLoginUrlJsonParameterName
+import com.salesforce.androidsdk.ui.LoginActivity.Companion.qrCodeLoginUrlPath
 import com.salesforce.androidsdk.ui.SalesforceActivity
+import com.salesforce.androidsdk.util.SalesforceSDKLogger.e
 import java.io.UnsupportedEncodingException
 import java.util.*
 
@@ -61,11 +69,6 @@ class MainActivity : SalesforceActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Check for and use the intent's QR code login URL if applicable.
-        if (isQrCodeLoginIntent(intent)) {
-            useQrCodeLoginUrl()
-        }
 
         // Set Theme
         val idDarkTheme = MobileSyncSDKManager.getInstance().isDarkTheme
@@ -90,21 +93,38 @@ class MainActivity : SalesforceActivity() {
 
     override fun onResume() {
         // Hide everything until we are logged in
-        findViewById<ViewGroup>(R.id.root).visibility = View.INVISIBLE
+        findViewById<ViewGroup>(root).visibility = View.INVISIBLE
 
         // Create list adapter
         listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, ArrayList())
         findViewById<ListView>(R.id.contacts_list).adapter = listAdapter
 
+        // Check for and use the intent's QR code login URL if applicable.
+        val buildRestClientOnResumePreviousValue = buildRestClientOnResume
+        if (isQrCodeLoginUrlIntent(intent ?: return)) {
+            /*
+             * Temporarily request the Salesforce Mobile SDK REST Client not be
+             * built on resume such that the default login activity will not
+             * display and conflict with the login activity for QR code log in.
+             *
+             * If the display of the default login activity was no longer a
+             * side-effect of getting the REST client, this variable could be
+             * removed.
+             */
+            buildRestClientOnResume = false
+            useQrCodeLogInUrl(intent.data ?: return)
+        }
+
         super.onResume()
+        buildRestClientOnResume = buildRestClientOnResumePreviousValue
     }
 
-    override fun onResume(client: RestClient) {
+    override fun onResume(client: RestClient?) {
         // Keeping reference to rest client
         this.client = client
 
         // Show everything
-        findViewById<ViewGroup>(R.id.root).visibility = View.VISIBLE
+        findViewById<ViewGroup>(root).visibility = View.VISIBLE
     }
 
     /**
@@ -184,12 +204,61 @@ class MainActivity : SalesforceActivity() {
 
     // region QR Code Login Via Salesforce Identity API UI Bridge Public Implementation
 
-    private fun useQrCodeLoginUrl() {
-        val uiBridgeApiParameters = uiBridgeApiParametersFromQrCodeLoginUrl(intent.data.toString())
-        SalesforceSDKManager.getInstance().apply {
-            frontDoorBridgeUrl = uiBridgeApiParameters?.frontdoorBridgeUrl
-            frontDoorBridgeCodeVerifier = uiBridgeApiParameters?.pkceCodeVerifier
+    /**
+     * Validates and uses the intent's QR code log in URL.
+     * @param url The QR code log in URL
+     */
+    private fun useQrCodeLogInUrl(url: Uri) {
+        val app = application as? MainApplication ?: return
+
+        // Use the specified QR code log in URL format.
+        if (app.isQrCodeLoginUsingReferenceUrlFormat) {
+
+            // Log in using `loginWithFrontdoorBridgeUrlFromQrCode` if applicable
+            if (url.scheme != app.qrCodeLoginUrlScheme
+                || url.host != app.qrCodeLoginUrlHost
+                || url.path != qrCodeLoginUrlPath
+                || !url.queryParameterNames.contains(qrCodeLoginUrlJsonParameterName)
+            ) {
+                e(javaClass.name, "Invalid QR code log in URL.")
+                return
+            }
+            startActivity(
+                Intent(
+                    this@MainActivity,
+                    LoginActivity::class.java
+                ).apply {
+                    data = url
+                })
+        } else {
+
+            /*
+             * When using `loginWithFrontdoorBridgeUrl` and an entirely custom
+             * QR code login URL format, set
+             * `isAppExpectedReferenceQrCodeLoginUrlFormat` to `false` (or
+             * remove it entirely) and implement URL handling in this block
+             * before calling `loginWithFrontdoorBridgeUrl`.
+             */
+
+            /* To-do: Implement URL handling to retrieve UI Bridge API parameters */
+            /* To set up QR code log in using `loginWithFrontdoorBridgeUrlFromQrCode`, provide the scheme and host for the expected QR code log in URL format */
+            val frontdoorBridgeUrl = "your-qr-code-login-frontdoor-bridge-url"
+            val pkceCodeVerifier = "your-qr-code-login-pkce-code-verifier"
+            check(frontdoorBridgeUrl != "your-qr-code-login-frontdoor-bridge-url") { "Please implement your app's frontdoor bridge URL retrieval." }
+            check(pkceCodeVerifier != "your-qr-code-login-pkce-code-verifier") { "Please add your app's PKCE code verifier retrieval if web server flow is used." }
+
+            startActivity(Intent(
+                this,
+                LoginActivity::class.java
+            ).apply {
+                putExtra(EXTRA_KEY_FRONTDOOR_BRIDGE_URL, frontdoorBridgeUrl)
+                putExtra(EXTRA_KEY_PKCE_CODE_VERIFIER, pkceCodeVerifier)
+                flags = FLAG_ACTIVITY_SINGLE_TOP
+            })
         }
+
+        // Clear the intent data so that the QR code log in URL is used only once.
+        intent.data = null
     }
 
     // endregion
