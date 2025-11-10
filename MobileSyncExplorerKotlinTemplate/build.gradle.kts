c725de137774ab12e2d515ecb6a56a65c042b42f
Eric C. Johnson <Johnson.Eric@Salesforce.com>
2025-11-10 15:29:27 -0700

    @W-20151632: MSDK Android Security Bug: CVE-2025-11953 - React Native Community CLI (RCE) (Updates To All Android Templates)

--- MobileSyncExplorerKotlinTemplate/build.gradle.kts
+++ MobileSyncExplorerKotlinTemplate/build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.10.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("com.android.tools.build:gradle:8.12.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
 
